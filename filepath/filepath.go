4504c5f2d4b09c43cfe2e895e457a21a5ec3d9d8
Kevin Yuan <farproc@gmail.com>
2023-03-22 00:43:58 +0800

    Add filepath

---
+++ filepath/filepath.go
@@ -0,0 +1,26 @@
+// Package filepath implements utility routines for manipulating filename paths.
+package filepath
+
+import (
+	"path/filepath"
+	"strings"
+)
+
+// Name returns the filename of path without extension.
+func Name(path string) string {
+	base := filepath.Base(path)
+	if dot := strings.LastIndexByte(base, '.'); dot != -1 {
+		return base[:dot]
+	}
+	return base
+}
+
+// ChangeExt returns a copy of path but with extension
+// changed to newExt. If path has no extension, newExt
+// is added as the extension.
+func ChangeExt(path, newExt string) string {
+	if newExt != "" && !strings.HasPrefix(newExt, ".") {
+		newExt = "." + newExt
+	}
+	return filepath.Join(filepath.Dir(path), Name(path)+newExt)
+}
---
+++ filepath/filepath_test.go
@@ -0,0 +1,73 @@
+// Package filepath implements utility routines for manipulating filename paths.
+package filepath
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestChangeExt(t *testing.T) {
+	type args struct {
+		path   string
+		newExt string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"normal", args{"a.txt", "html"}, "a.html"},
+		{"no ext", args{"abc", ".c"}, "abc.c"},
+		{"empty new ext", args{"abc", ""}, "abc"},
+		{"multi dots", args{"/path/a.b/c.e", "f"}, "/path/a.b/c.f"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ChangeExt(tt.args.path, tt.args.newExt); got != tt.want {
+				t.Errorf("ChangeExt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleChangeExt() {
+	fmt.Println(ChangeExt("a.txt", "html"))
+	fmt.Println(ChangeExt("/path/file", ".c"))
+	fmt.Println(ChangeExt("a.txt", ""))
+	fmt.Println(ChangeExt(".a", "txt"))
+	// Output:
+	// a.html
+	// /path/file.c
+	// a
+	// .txt
+}
+
+func TestName(t *testing.T) {
+	tests := []struct {
+		name string
+		arg  string
+		want string
+	}{
+		{"normal", "a.txt", "a"},
+		{"path", "/path/a.txt", "a"},
+		{"multi dots", "/path/a.b.txt", "a.b"},
+		{"empty name", ".x", ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Name(tt.arg); got != tt.want {
+				t.Errorf("Name() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleName() {
+	fmt.Println(Name("a.txt"))
+	fmt.Println(Name("/path/b.c.e"))
+	fmt.Printf("%#v", Name(".c"))
+	// Output:
+	// a
+	// b.c
+	// ""
+}
