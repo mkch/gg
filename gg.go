8d17c81281f644e1e35c5faada623fc5bf2d2fcd
Kevin Yuan <farproc@gmail.com>
2024-01-26 17:49:04 +0800

    Add IfFunc and MustOK

--- gg.go
+++ gg.go
@@ -1,7 +1,7 @@
 // Package gg is a set of useful golang utilities.
 package gg
 
-// If returns truePart of cond is true, or returns falsePart.
+// If returns truePart if cond is true, or returns falsePart.
 // Something like ternary operator in C:
 //
 //	cond ? truePart : falsePart
@@ -13,6 +13,21 @@ func If[T any](cond bool, truePart T, falsePart T) T {
 	}
 }
 
+// IfFunc calls truePart() and returns it's return value if cond is true,
+// or calls falsePart() and returns it's return value.
+// If cond is true, falsePart will not be executed.
+// if cond if false, truePart will not be executed.
+// Something like ternary operator in C:
+//
+//	cond ? truePart() : falsePart()
+func IfFunc[T any](cond bool, truePart func() T, falsePart func() T) T {
+	if cond {
+		return truePart()
+	} else {
+		return falsePart()
+	}
+}
+
 // Must returns v if err is nil, or it panic with err.
 // Must is useful to wrap a function call returning value and error
 // when there is no better way to handle the error other than panicking.
@@ -21,4 +36,11 @@ func Must[T any](v T, err error) T {
 		panic(err)
 	}
 	return v
+}
+
+// MustOK panics if err is not nil.
+func MustOK(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
--- gg_test.go
+++ gg_test.go
@@ -1,7 +1,9 @@
 package gg_test
 
 import (
+	"errors"
 	"fmt"
+	"os"
 	"strconv"
 	"testing"
 
@@ -18,8 +20,52 @@ func TestIf(t *testing.T) {
 	}
 }
 
+func TestIfFunc(t *testing.T) {
+	var (
+		truePartExec, falsePareExec bool
+	)
+
+	if v := gg.IfFunc(
+		true,
+		func() string {
+			truePartExec = true
+			return "true"
+		},
+		func() string {
+			falsePareExec = true
+			return "false"
+		}); v != "true" {
+		t.Fatal(v)
+	} else if !truePartExec {
+		t.Fatal("true part should be executed")
+	} else if falsePareExec {
+		t.Fatal("false pare should not be executed")
+	}
+}
+
 func ExampleMust() {
 	fmt.Print(gg.Must(strconv.Atoi("1")))
 	// Output:
 	// 1
+}
+
+func ExampleMustOK() {
+	gg.MustOK(os.Setenv("some_key_for_test", "some value"))
+}
+
+func TestMustOK(t *testing.T) {
+	errInvalidArgument := errors.New("must >= 0")
+	defer func() {
+		if err := recover(); err != errInvalidArgument {
+			t.Fatal(err)
+		}
+	}()
+	greaterThanZero := func(i int) error {
+		if i < 0 {
+			return errInvalidArgument
+		}
+		return nil
+	}
+
+	gg.MustOK(greaterThanZero(-1))
 }
